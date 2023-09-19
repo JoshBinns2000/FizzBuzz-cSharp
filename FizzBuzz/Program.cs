2ff852df1b513a936219d10d229e0c5963fbae22
Joshua Binns <Joshua.Binns@softwire.com>
2023-09-19 17:25:07 +0100

    Working (non optimised) version

--- FizzBuzz/Program.cs
+++ FizzBuzz/Program.cs
@@ -1,3 +1,16 @@
 ﻿// See https://aka.ms/new-console-template for more information
 
-Console.WriteLine("Hello, World!");
+using System.Runtime.CompilerServices;
+
+for (int i = 1; i < 101; i++)
+{
+    bool isFizz = i % 3 == 0;
+    bool isBuzz = i % 5 == 0;
+    
+    int result = Convert.ToInt32(isFizz) + Convert.ToInt32(isBuzz);
+    
+    if (result == 2) Console.WriteLine("FizzBuzz");
+    else if (isFizz) Console.WriteLine("Fizz");
+    else if (isBuzz) Console.WriteLine("Buzz");
+    else Console.WriteLine(i);
+}
