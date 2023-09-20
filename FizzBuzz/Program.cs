d60714cfd1ac5e03d3248f756231fb4af253fd98
Joshua Binns <Joshua.Binns@softwire.com>
2023-09-20 09:50:50 +0100

    Rules up to 11 (format stops helping now)

--- FizzBuzz/Program.cs
+++ FizzBuzz/Program.cs
@@ -1,22 +1,43 @@
 ﻿// See https://aka.ms/new-console-template for more information
 
+using System.Drawing;
 using System.Runtime.CompilerServices;
 
 Dictionary<int, string> rules = new Dictionary<int, string>();
 
-rules.Add(3, "Fizz");
+int[] priorityRules =
+{
+    11, // Bong
+    3 * 5 * 7, // FizzBuzzBang
+    3 * 7, // FizzBang
+    5 * 7, // BuzzBang
+    3 * 5, // FizzBuzz
+    7, // Bang
+    5, // Buzz
+    3, // Fizz
+};
+
+rules.Add(3 * 5 * 7, "FizzBuzzBang");
+rules.Add(3 * 7, "FizzBang");
+rules.Add(5 * 7, "BuzzBang");
+rules.Add(3 * 5, "FizzBuzz");
+rules.Add(11, "Bong");
+rules.Add(7, "Bang");
 rules.Add(5, "Buzz");
+rules.Add(3, "Fizz");
 
 for (int i = 1; i < 101; i++)
 {
     bool emptyOutput = true;
 
-    foreach (KeyValuePair<int, string> rule in rules)
+    foreach (int ruleValue in priorityRules)
     {
-        if (i % rule.Key == 0)
+        if (i % ruleValue == 0)
         {
-            Console.Write(rule.Value);
+            string output = rules[ruleValue]; // no need to protect against bad inputs obviously
+            Console.Write(output);
             emptyOutput = false;
+            break;
         }
     }
     
